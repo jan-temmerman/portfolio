6e34771b26c589a64466a520b3012da9b0996ffb
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-04-28 14:28:01 +0200

    first push

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,17 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+import headerStyles from "./header.module.css"
+
+export default() => (
+  <header>
+    <div className={headerStyles.container}>
+    <h1 className={headerStyles.logo}>JAN TEMMERMAN</h1>
+      <div className={headerStyles.linkContainer}>
+        <Link className={headerStyles.link} to="/">Home</Link>
+        <Link className={headerStyles.link} to="/">Contact</Link>
+        <Link className={headerStyles.link} to="/">About</Link>
+      </div>
     </div>
   </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+) 
