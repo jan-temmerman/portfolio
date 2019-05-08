f92ca50f630302871b7a9b125e0ed16e9a623d89
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-05-08 15:22:52 +0200

    404 page added

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+
+import contactStyle from "./contact.module.css"
+
+import sw404 from "../images/sw404.gif"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+<Layout>
+  <section className="centered">
+    <div className={contactStyle.container}>
+      <h1 className={contactStyle.greet}>ERROR 404</h1>
+      <p className={contactStyle.intro}>You're not supposed to be here...</p>
+      <img src={sw404} alt="Computer man" className={contactStyle.notFound}></img>
+    </div>
+  </section>
+</Layout>
 )
 
 export default NotFoundPage
