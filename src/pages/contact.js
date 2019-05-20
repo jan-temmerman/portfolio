587856a7f685f72cd572a37e91bb169112bc8f16
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-05-20 20:46:52 +0200

    +mail button

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,10 +17,12 @@ const IndexPage = () => (
         <h1 className={contactStyle.greet}>Contact</h1>
         <p className={contactStyle.intro}>Feel free to send me a mail, message me on Facebook or check out my projects on Github!</p>
         <table>
-            <tr>
-                <td>Mail</td>
-                <td className={contactStyle.contact}>temmjan@gmail.com</td>
-            </tr>
+          <tr>
+              <td>Mail</td>
+              <a href="mailto:temmjan@gmail.com" className={contactStyle.contact}>
+                <td>temmjan@gmail.com</td>
+              </a>
+          </tr>
         </table>
         <div className={contactStyle.iconContainer}>
             <a href="https://www.linkedin.com/in/jan-temmerman-896b07146/">
