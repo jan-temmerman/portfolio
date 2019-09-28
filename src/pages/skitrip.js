38e9bcc5626b3b7faa0945d7f450ecc7a75d091f
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-09-29 01:34:32 +0200

    fixed bug with checking 'window' when not mounted

--- src/pages/skitrip.js
+++ src/pages/skitrip.js
@@ -6,19 +6,48 @@ import Layout from "../components/layout"
 
 import contactStyle from "./contact.module.css"
 
-const SkitripPage = () => (
-  <Layout>
-    <SEO title="Contact" keywords={[`Jan Temmerman`, `portfolio`, `skitrip`]} description="Portfolio website of jan temmerman, skitrip"/>
-    <section className="centered">
-      <div className={contactStyle.container}>
-        <h1 className={contactStyle.greet}>Fresh pow in Flaine</h1>
-        <CountDown
-                end="Februari 1, 2020"
-            />
-        <div className={contactStyle.photo}></div>
-      </div>
-    </section>
-  </Layout>
-)
+class SkitripPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mounted: false
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            mounted: true
+        })
+    }
+
+    render() {
+        if(this.state.mounted) {
+            return(
+                <Layout>
+                    <SEO title="Contact" keywords={[`Jan Temmerman`, `portfolio`, `skitrip`]} description="Portfolio website of jan temmerman, skitrip"/>
+                    <section className="centered">
+                    <div className={contactStyle.container}>
+                        <h1 className={contactStyle.greet}>Fresh pow in Flaine</h1>
+                        <CountDown end='Februari 1, 2020'/>
+                        <div className={contactStyle.photo}></div>
+                    </div>
+                    </section>
+                </Layout>
+            )
+        } else {
+            return(
+                <Layout>
+                    <SEO title="Contact" keywords={[`Jan Temmerman`, `portfolio`, `skitrip`]} description="Portfolio website of jan temmerman, skitrip"/>
+                    <section className="centered">
+                    <div className={contactStyle.container}>
+                        <h1 className={contactStyle.greet}>Fresh pow in Flaine</h1>
+                        <div className={contactStyle.photo}></div>
+                    </div>
+                    </section>
+                </Layout>
+            )
+        }
+    }
+}
 
 export default SkitripPage
